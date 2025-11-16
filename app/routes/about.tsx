47f9598ff47d9833daa29479c913706a2b67f71c
twoamdev <97489062+twoamdev@users.noreply.github.com>
2025-11-16 15:11:19 -0500

    first commit with react and router7 build

---
+++ app/routes/about.tsx
@@ -0,0 +1,12 @@
+import type { Route } from "./+types/about";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function About() {
+  return <div className="p-8">About Page</div>;
+}
