a6a125f0db71fc9683a631306272567fd8d6bd40
twoamdev <97489062+twoamdev@users.noreply.github.com>
2025-11-16 15:19:56 -0500

    added netlify support

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+import netlifyReactRouter from "@netlify/vite-plugin-react-router";
+
+
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
